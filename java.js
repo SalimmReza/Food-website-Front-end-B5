2a1bd4cdfbc9c234789545b513bc95704aa80c1b
SalimmReza <refath300@gmail.com>
2022-01-03 21:42:24 +0600

    banner and count

---
+++ java.js
@@ -0,0 +1,54 @@
+//----------------------------/counter design
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() =>{
+            current +=increment;
+            obj.textContent = current;
+            if(current == end)
+            {
+                clearInterval(timer)
+            }
+
+        }, step);
+
+    }
+
+    counter("count1", 0,1234, 4321);
+    counter("count2", 0,1294, 6721);
+    counter("count3", 8,834, 8721);
+    counter("count4", 0,1084, 6521);
+});
+
+//------------------------------navbar
+
+let nav= document.querySelector(".navigation-wrap");
+window.onscroll = function()
+{
+    if(document.documentElement.scrollTop > 20)
+    {
+        nav.classList.add("scroll-on");
+    }else
+    {
+            nav.classList.remove("scroll-on");
+    }
+}
+
+
+//------------------------------navbar hide
+
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a)
+{
+    a.addEventListener("click" , function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
